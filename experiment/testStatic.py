9bf120767be2c1bbafb021150b1cb6845e33b468
= <somnuk.wk@gmail.com>
2014-06-24 17:43:47 +0700

    add dynamic import

---
+++ experiment/testStatic.py
@@ -0,0 +1,19 @@
+
+import unittest
+
+class Test(object) :
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def run() :
+        print "Hello"
+        return True
+
+
+
+class TestStatic(unittest.TestCase) :
+
+    def testStatic(self) :
+        rs = Test.run()
+        self.assertEqual(rs, True)
